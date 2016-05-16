3e585540925dd9519d37dd921b4a2ef47c8360c6
Scott Kidder <scott@kidder.io>
2016-05-16 14:29:30 -0700

    Remove reference to GOPATH

--- run.sh
+++ run.sh
@@ -2,7 +2,4 @@
 set -e
 
 debug "$("$WERCKER_STEP_ROOT/glide" --version)"
-info "using GOPATH=$("$WERCKER_STEP_ROOT/glide" gopath)"
-
-export GOPATH=$("$WERCKER_STEP_ROOT/glide" gopath)
-"$WERCKER_STEP_ROOT/glide" install
+"$WERCKER_STEP_ROOT/glide" install --cache
